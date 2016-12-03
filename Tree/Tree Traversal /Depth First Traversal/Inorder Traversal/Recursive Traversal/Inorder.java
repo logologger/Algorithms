bbff82d175e61fb6f280cfff00ccb2c0f0f360e5
razzcoder <rajatsbhadauria@gmail.com>
2016-12-03 12:55:42 +0530

    Traversal Folder created

---
+++ Tree/Tree Traversal /Depth First Traversal/Inorder Traversal/Recursive Traversal/Inorder.java
@@ -0,0 +1,85 @@
+import java.io.*;
+import java.util.*;
+class Node{
+	int data;
+	Node left,right;
+	Node(int item){
+		data=item;
+	}
+}
+
+class Inorder{
+	Node root;
+	
+	Inorder(){
+				root=null;
+	}
+	// public void printPostOrder(Node node){
+	// 	if(node==null){
+	// 		return ;
+	// 	}
+	// 	printPostOrder(node.left);
+
+	// 	printPostOrder(node.right);
+		
+	// 	System.out.print(" "+node.data);
+	// }
+
+	// public void printPreOrder(Node node){
+	// 	if(node==null){
+	// 		return ;
+	// 	}
+	// 	System.out.print(" "+node.data);
+
+	// 	printPreOrder(node.left);
+
+	// 	printPreOrder(node.right);
+	// }
+
+	public void printInorder(Node node){
+		if(node==null){
+			return ;
+		}
+
+		printInorder(node.left);
+
+		System.out.println(node.data);
+
+		printInorder(node.right);
+	}
+
+	void printInOrder(){
+				printInorder(root);
+	}
+	// void printPreOrder(){
+	// 			printPreOrder(root);
+
+	// }
+	// void printPostOrder(){
+	// 			printPostOrder(root);
+	// }
+
+
+
+	public static void main(String args[]){
+
+			Inorder tree = new Inorder();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+ 
+        // System.out.println("Preorder traversal of binary tree is ");
+        // tree.printPreOrder();
+ 
+        System.out.println("\nInorder traversal of binary tree is ");
+        tree.printInOrder();
+ 
+        // System.out.println("\nPostorder traversal of binary tree is ");
+        // tree.printPostOrder();
+	}
+
+
+
+}
