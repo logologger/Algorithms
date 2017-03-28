bd3106b555dc54dd8abfe3b839df033fd1c18962
razzcoder <rajatsbhadauria@gmail.com>
2017-03-28 20:55:56 +0530

    ADded HashTable Second Question

---
+++ HackerEarth Practice Problems/HashTable/Maximumoccurence.java
@@ -0,0 +1,216 @@
+
+import java.util.*;
+import java.io.*;
+ 
+class Maximumoccurence {
+    public static void main(String args[] ) throws Exception {
+       
+ 
+        //Scanner
+        Print print = new Print();
+        Scan s = new Scan();
+        
+        InputStreamReader reader=new InputStreamReader(System.in);
+        BufferedReader br=new BufferedReader(reader);
+        String str=br.readLine();
+
+        Hashtable<Character,Integer> hs=new Hashtable<Character,Integer>();
+        for(int i=0;i<str.length();i++){
+
+            char ch=str.charAt(i);
+
+            hs.put(ch,hs.get(ch)!=null ? hs.get(ch)+1 : 1);
+
+
+
+         
+    }
+    //Iterate through the hashTable and give counts for all the characters
+
+    int highestValue=0;
+    int start_char=256;
+
+    char highestKey=(char)start_char;
+
+    for(Map.Entry m : hs.entrySet()){
+
+        char currentChar=Character.valueOf((Character)m.getKey());
+        int currentValue=Integer.valueOf((Integer)m.getValue());
+
+      //  System.out.println(currentValue+" "+currentChar);
+        
+         if(currentValue>highestValue){
+
+             highestValue=currentValue;
+             highestKey=currentChar;
+
+         }
+    }
+
+    System.out.println(highestKey+" "+highestValue);
+        
+
+        }
+}
+ 
+class Scan {
+    private byte[] buf=new byte[1024];
+    private int index;
+    private InputStream in;
+    private int total;
+    public Scan() {
+        in=System.in;
+    }
+ 
+    public int scan()throws IOException {
+        if(total<0) {
+            throw new InputMismatchException();
+        }
+        if(index>=total) {
+            index=0;
+            total=in.read(buf);
+            if(total<=0) {
+                return -1;
+            }
+        }
+        return buf[index++];
+    }
+ 
+    public int scanInt()throws IOException {
+        int integer=0;
+        int n=scan();
+        while(isWhiteSpace(n)) {
+            n=scan();
+        }
+        int neg=1;
+        if(n=='-') {
+            neg=-1;
+            n=scan();
+        }
+        while(!isWhiteSpace(n)) {
+            if(n>='0'&&n<='9') {
+                integer*=10;
+                integer+=n-'0';
+                n=scan();
+            }
+            else throw new InputMismatchException();
+        }
+        return neg*integer;
+    }
+ 
+    public long scanLong()throws IOException {
+        long integer=0;
+        int n=scan();
+        while(isWhiteSpace(n)) {
+            n=scan();
+        }
+        int neg=1;
+        if(n=='-') {
+            neg=-1;
+            n=scan();
+        }
+        while(!isWhiteSpace(n)) {
+            if(n>='0'&&n<='9') {
+                integer*=10;
+                integer+=n-'0';
+                n=scan();
+            }
+            else throw new InputMismatchException();
+        }
+        return neg*integer;
+    }
+ 
+    public double scanDouble()throws IOException {
+        double doub=0;
+        int n=scan();
+        while(isWhiteSpace(n))
+            n=scan();
+        int neg=1;
+        if(n=='-') {
+            neg=-1;
+            n=scan();
+        }
+        while(!isWhiteSpace(n)&&n!='.') {
+            if(n>='0'&&n<='9') {
+                doub*=10;
+                doub+=n-'0';
+                n=scan();
+            }
+            else throw new InputMismatchException();
+        }
+        if(n=='.') {
+            n=scan();
+            double temp=1;
+            while(!isWhiteSpace(n)) {
+                if(n>='0'&&n<='9') {
+                    temp/=10;
+                    doub+=(n-'0')*temp;
+                    n=scan();
+                }
+                else throw new InputMismatchException();
+            }
+        }
+        return doub*neg;
+    }
+ 
+    public String scanString()throws IOException {
+        StringBuilder sb=new StringBuilder();
+        int n=scan();
+        while(isWhiteSpace(n)) {
+            n=scan();
+        }
+        while(!isWhiteSpace(n)) {
+            sb.append((char)n);
+            n=scan();
+        }
+        return sb.toString();
+    }
+ 
+    public byte[] scanBytes(int N)throws IOException {
+        byte[] bytes = new byte[N];
+        int diff = total-index;
+        int i=index;
+        for (;i<total && i-index < N; i++) {
+            bytes[i-index] = buf[i];
+        }
+        index = i;
+        if (diff >= N) {
+            return bytes;
+        }
+        in.read(bytes, diff, N-diff);
+        return bytes;
+    }
+ 
+    public boolean scanBoolean() throws IOException {
+        int integer=0;
+        int n=scan();
+        while(isWhiteSpace(n)) {
+            n=scan();
+        }
+        return n == '1';
+    }
+ 
+    private boolean isWhiteSpace(int n) {
+        if(n==' '||n=='\n'||n=='\r'||n=='\t'||n==-1) {
+            return true;
+        }
+        return false;
+    }
+}
+ 
+class Print {
+    private final BufferedWriter bw;
+    public Print() {
+        bw=new BufferedWriter(new OutputStreamWriter(System.out));
+    }
+    public void print(String str)throws IOException {
+        bw.append(str);
+    }
+    public void printLine(String str)throws IOException {
+        print(str);
+        bw.append("\n");
+    }
+    public void close()throws IOException {
+        bw.close();
+    }
+}
